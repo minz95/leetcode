881cfeaae5e01390224961a39056ce4a125cfce7
Kim Minji <kimmj.95@gmail.com>
2025-01-28 22:32:14 +0900

    Time: 135 ms (63.33%), Space: 78.3 MB (59.46%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.map = {}
+        self.capacity = capacity
+        self.head = Node(0, 0)  
+        self.tail = Node(0, 0)  
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _add_to_head(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.map:
+            node = self.map[key]
+            self._remove(node)
+            self._add_to_head(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.map:
+            node = self.map[key]
+            node.value = value
+            self._remove(node)
+            self._add_to_head(node)
+        else:
+            if len(self.map) >= self.capacity:
+                lru = self.tail.prev
+                self._remove(lru)
+                del self.map[lru.key]
+            n_node = Node(key, value)
+            self.map[key] = n_node
+            self._add_to_head(n_node)
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
