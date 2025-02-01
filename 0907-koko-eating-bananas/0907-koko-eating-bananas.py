50855fb33a114fa3512b32053197d98c83903544
Kim Minji <kimmj.95@gmail.com>
2025-02-01 23:40:30 +0900

    Time: 167 ms (37.93%), Space: 19 MB (23.86%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -4,15 +4,13 @@ class Solution:
         piles.sort()
         end = max(piles)
         start = max(1, sum(piles) // h)
-        ans = end
-        while start <= end:
+        while start < end:
             mid = (start + end) // 2
             eath = sum([math.ceil(i / mid) for i in piles])
             if eath > h:
-                start = mid + 1
-            elif eath <= h:
-                ans = mid
-                end = mid - 1
-        return ans
+                start = mid+1
+            else:
+                end = mid
+        return start
 
             
