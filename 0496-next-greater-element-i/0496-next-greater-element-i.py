1f1c15376452bc6e8c104b05e840b864f34188d1
Kim Minji <kimmj.95@gmail.com>
2025-02-01 22:37:57 +0900

    Time: 1 ms (54.58%), Space: 18.2 MB (17.25%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,14 +1,14 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        result = []
-        for i in nums1:
-            found = False
-            for idx, j in enumerate(nums2):
-                if found and j > i:
-                    result.append(j)
-                    break
-                if j == i:
-                    found = True
-                if idx == len(nums2) - 1:
-                    result.append(-1)
-        return result
+        stack = []
+        hmap = {}
+
+        for num in nums2:
+            while stack and num > stack[-1]:
+                hmap[stack.pop()] = num
+            stack.append(num)
+
+        while stack:
+            hmap[stack.pop()] = -1
+
+        return [hmap.get(i, -1) for i in nums1]    
